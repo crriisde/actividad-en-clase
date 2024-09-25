02dd1578fea4cad49d8abf88e6e666fcef08d96c
crriisde <a.23308051281214@cbtis128.edu.mx>
2024-09-24 20:14:41 -0600

    Add files via upload

---
+++ multiplica3numeros_Salas/multiplica3numeros_salas.py
@@ -0,0 +1,16 @@
+print (" ") #esta variable muestra las comillas para el texto
+print ("Salas De La O Cristian David : Mi practica de clase string.py") #esta variable mostrara en la pantalla los datos 
+#de la practica
+print (" ") #esta variable muestra las comillas para el texto 
+
+def multiplicacion(a, b, c): #esta linea define las 3 variables a multiplicar
+    return a * b * c #esta linea indica el final de la funcion
+
+a = int(input('Pon el primer numero a multiplicar: ')) #esta solicita y define la variable "a"
+b = int(input('Pon el segundo numero a multiplicar: ')) #esta solicita y define la variable "b"
+c = int(input('Pon el tercer numero a multiplicar: ')) #esta solicita y define la variable "c"
+
+resultado = multiplicacion (a, b, c) #esta linea define la multiplicacion de los 3 numeros
+
+print ("el resultado es:") #esta linea avisa que lo siguiente a moestrar es el resultado de la multiplicacion
+print (resultado) #esta linea muestra el resultado de la multiplicacion de los 3 numeros 
